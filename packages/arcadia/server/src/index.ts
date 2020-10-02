22fc29a21c96c5ee162b93136c2dea3bc5a5c957
nastushalk <nastusha.leve@gmail.com>
2020-10-02 17:46:00 +0300

    feat: add graphql

--- packages/arcadia/server/src/index.ts
+++ packages/arcadia/server/src/index.ts
@@ -1,11 +1,24 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import { ApolloServer } from 'apollo-server-express'
+
+import { genSchema } from './lib/gen-schema'
 
 const app = express()
 const port = 5000
 
+const server = new ApolloServer({
+  schema: genSchema(),
+  context: ({ req, res }) => ({
+    req,
+    res,
+  }),
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+server.applyMiddleware({ app, path: '/graphql' })
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`)
