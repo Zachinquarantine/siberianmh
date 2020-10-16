9c2af4b646dba8bb030c1cd53d43fb7d32a85d62
nastushalk <nastusha.leve@gmail.com>
2020-10-16 22:28:05 +0300

    fix: use only process.env.REDIS_URL

--- packages/arcadia/server/src/lib/redis.ts
+++ packages/arcadia/server/src/lib/redis.ts
@@ -1,6 +1,5 @@
 import * as Redis from 'ioredis'
 
-export const redis =
-  process.env.REDIS_URL || process.env.NODE_ENV !== 'development'
-    ? new Redis(process.env.REDIS_URL)
-    : new Redis()
+export const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis()
