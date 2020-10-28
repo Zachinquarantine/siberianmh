65ebc7c955e417992f41e0fdbf88b87f6c461590
vhashimotoo <hashimoto.stream@gmail.com>
2020-10-28 22:49:25 +0200

    chore: fixup role inhibitoring

--- packages/uwulectron-discord-bot/src/lib/inhibitors.ts
+++ packages/uwulectron-discord-bot/src/lib/inhibitors.ts
@@ -8,8 +8,8 @@ export const isTrustedMember: Inhibitor = async (msg) => {
   }
 
   if (
-    !msg.member.roles.cache.has(trustedRoleId) ||
-    !msg.member.hasPermission('MANAGE_MESSAGES')
+    !msg.member.hasPermission('MANAGE_MESSAGES') &&
+    !msg.member.roles.cache.has(trustedRoleId)
   ) {
     // eslint-disable-next-line
     return ":warning: you don't have permissions to use that command."
