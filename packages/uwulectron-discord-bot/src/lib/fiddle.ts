7f3645f83081d7559fa35e73103e742ab29c0722
nastushalk <nastusha.leve@gmail.com>
2020-11-10 21:23:04 +0200

    refactor(uwulectron): remove fiddle module

--- packages/uwulectron-discord-bot/src/lib/fiddle.ts
+++
@@ -1,5 +0,0 @@
-export const GIST_REGEX = /https?:\/\/(?:www\.)?gist.github\.com\/([0-9a-f]{32})$/gm
-
-export const generateFiddleRunURL = (gistId: string) => {
-  return `https://fiddle.electronjs.org/launch?target=gist/${gistId}`
-}
