7a8fa12666aed08f24e1683959508de9a4ecf263
nastushalk <nastusha.leve@gmail.com>
2020-12-04 07:42:39 -0800

    refactor(subrepos): remove old manager

--- packages/subrepos/src/index.ts
+++ packages/subrepos/src/index.ts
@@ -1,17 +1,15 @@
 import * as yargs from 'yargs'
 import { listOutdated } from './lib/commands/outdated'
-import { Manager } from './manager'
-
-const manager = new Manager()
+import { install } from './lib/commands/install'
 
 export const run = () => {
   // eslint-disable-next-line
   yargs
     .scriptName('subrepos')
     .usage('$0 <cmd> [args]')
-    .command('install', 'Install the subrepositories', () => manager.runFetch())
+    .command('install', 'Install the subrepositories', () => install())
     .command('outdated', 'Show the outdated subrepositories', listOutdated)
-    .command('*', 'Install the subrepositories.', () => manager.runFetch())
+    .command('*', 'Install the subrepositories.', () => install())
     .showHelpOnFail(true)
     .demandCommand(1, '')
     .help().argv
---
+++ packages/subrepos/src/lib/commands/install.ts
@@ -0,0 +1,52 @@
+import * as findUp from 'find-up'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import { clone } from '../clone'
+import { parseYML } from '../parser'
+
+export const install = async () => {
+  const subreposFile = (await findUp('subrepos.yml'))!
+  const content = parseYML(subreposFile)
+
+  return content.map((repo) => {
+    if (fs.existsSync(path.join(process.cwd(), repo.directory))) {
+      return
+    }
+
+    return clone(repo, process.cwd())
+  })
+}
+
+// TODO: Modify to a new version
+// const verifyContent(subrepos: Array<ISubrepos>) {
+//   let hasError: boolean = false
+
+//   const log = (subrepo: ISubrepos, message: string) => {
+//     if (!subrepo.name) {
+//       return console.log(`[${subrepo.name}] ${message}`)
+//     }
+
+//     return console.log(message)
+//   }
+
+//   subrepos.forEach((subrepo) => {
+//     if (!subrepo.commit) {
+//       log(subrepo, 'Unable to find required field "commit"')
+//       hasError = true
+//     }
+//     if (!subrepo.directory) {
+//       log(subrepo, 'Unable to find required field "directory"')
+//       hasError = true
+//     }
+//     if (!subrepo.url) {
+//       log(subrepo, 'Unable to find required field "url"')
+//       hasError = true
+//     }
+//   })
+
+//   if (hasError) {
+//     return true
+//   }
+
+//   return false
+// }
